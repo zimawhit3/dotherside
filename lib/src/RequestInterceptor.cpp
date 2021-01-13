d5e62ec07b32964af7fbd7d6c2caf12a87a53b04
Richard Ramos <info@richardramos.me>
2021-01-13 11:25:26 -0400

    block a request

--- lib/src/RequestInterceptor.cpp
+++ lib/src/RequestInterceptor.cpp
@@ -38,14 +38,10 @@ void RequestInterceptor::interceptRequest(QWebEngineUrlRequestInfo & info)
 {
     // Intercepting the requested URL
     QUrl url = info.requestUrl();
-    qDebug() << ">>>>>>>>>>>>>>>>>>>>>>>>>Request URL: " << url;
+    qDebug() << ">>> Requested URL: " << url;
 
-    // Optionally redirect the request URL but it only works for requests 
-    // without payload data such as GET ones
-    //info.redirect(QUrl("https://www.google.com"));
-
-    // Set HTTP header
-    //QByteArray httpHeaderName = "SomeHeaderName";
-   // QByteArray httpHeaderValue = "SomeHeaderValue";
-    //info.setHttpHeader(httpHeaderName, httpHeaderValue);
+    if (url.fileName() == QString("ThisIsNotAllowed.js")){
+        info.block(true);
+        qDebug() << "URL has been blocked: " << url;
+    }
 }
